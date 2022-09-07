a00722a6f7561f87cf1cb548666d3e982bbdc09e
Rafael Júnior Ribeiro <rafael.ribeiro@contaquanto.com.br>
2022-09-07 19:21:39 -0300

    added kfold split

--- k_fold/src/Kfold/kfold.py
+++ k_fold/src/Kfold/kfold.py
@@ -2,6 +2,10 @@ import random
 from typing import Generator, Set, Tuple, Any
 import pickle
 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
 random.seed(10)
 
 
@@ -9,8 +13,9 @@ class Kfold:
     def __init__(self, k: int, shuffle=True):
         self.k = k
         self.shuffle = shuffle
+        self.splits = self.generate_splits()
         
-    def generate_splits(self) -> Generator[tuple[tuple[int], int], None, None]:
+    def generate_splits(self) -> set[tuple[int], int]:
         splits = []
         for index_fold_test in range(self.k):
             folds_for_training = []
@@ -19,10 +24,10 @@ class Kfold:
                 if index_fold_train != index_fold_test:
                     folds_for_training.append(index_fold_train)
     
-        splits = ((tuple(folds_for_training), fold_for_test) for folds_for_training, fold_for_test in splits)
+        splits = set((tuple(folds_for_training), fold_for_test) for folds_for_training, fold_for_test in splits)
         return splits
     
-    def generate_folds(self, y_values: list[int]) -> set[tuple[int]]:
+    def generate_folds(self, y_values: np.ndarray[int]) -> list[tuple[int]]:
         indexes_with_false_label = [index for index, value in enumerate(y_values) if value == 0]
         indexes_with_true_label = [index for index, value in enumerate(y_values) if value == 1]
         if self.shuffle:
@@ -38,11 +43,36 @@ class Kfold:
             except IndexError:
                 break
 
-        return set([tuple(fold) for fold in folds])
+        return [tuple(fold) for fold in folds]
 
-#    def split(self, X, y):
+    def split(self, X: np.ndarray, y: np.ndarray[int]):
+        folds = self.generate_folds(y_values=y)
+        X_train = []
+        y_train = []
+        X_test = []
+        y_test = []
+        for train_indexes_folds, test_index_fold in self.splits:
+            for train_index in train_indexes_folds:
+                X_train.extend(X.take(folds[train_index], axis=0))
+                y_train.extend(y.take(folds[train_index], axis=0))
+            X_test.extend(X.take(folds[test_index_fold], axis=0))
+            y_test.extend(y.take(folds[test_index_fold], axis=0))
 
+            yield X_train, X_test, y_train, y_test
+
+
+def main():
+    df = pd.read_csv("../../data/winequality-red.csv",
+                     sep=";")
+    y = df.quality.apply(lambda quality: 0 if quality <= 5 else 1).values
+    X = MinMaxScaler().fit_transform(df.iloc[:, :-1])
+
+    # for index in [3, 5, 8]:
+    #     kfold = Kfold(index)
+    #     folds = kfold.generate_folds(y)
+    #     with open(f"../../tests/mocks/values_splitted_in_{index}_folds", "wb") as file:
+    #         pickle.dump(folds, file)
 
 
 if __name__ == "__main__":
-    kfold = Kfold(3)
+    main()
