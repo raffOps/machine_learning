751c7c6459bc9d9c9f0e03b2d0fd9af41bf973d0
Rafael Júnior Ribeiro <rafael.ribeiro@contaquanto.com.br>
2022-09-10 18:59:40 -0300

    added random forest tunning

--- k_fold/src/Kfold/kfold.py
+++ k_fold/src/Kfold/kfold.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-random.seed(10)
+random.seed(42)
 
 
 class Kfold:
@@ -27,7 +27,7 @@ class Kfold:
         splits = set((tuple(folds_for_training), fold_for_test) for folds_for_training, fold_for_test in splits)
         return splits
     
-    def generate_folds(self, y_values: np.ndarray[int]) -> list[tuple[int]]:
+    def generate_folds(self, y_values: np.ndarray) -> list[tuple[int]]:
         indexes_with_false_label = [index for index, value in enumerate(y_values) if value == 0]
         indexes_with_true_label = [index for index, value in enumerate(y_values) if value == 1]
         if self.shuffle:
@@ -45,20 +45,18 @@ class Kfold:
 
         return [tuple(fold) for fold in folds]
 
-    def split(self, X: np.ndarray, y: np.ndarray[int]):
+    def split(self, x: np.ndarray, y: np.ndarray):
         folds = self.generate_folds(y_values=y)
-        X_train = []
-        y_train = []
-        X_test = []
-        y_test = []
         for train_indexes_folds, test_index_fold in self.splits:
+            x_train = []
+            y_train = []
             for train_index in train_indexes_folds:
-                X_train.extend(X.take(folds[train_index], axis=0))
+                x_train.extend(x.take(folds[train_index], axis=0))
                 y_train.extend(y.take(folds[train_index], axis=0))
-            X_test.extend(X.take(folds[test_index_fold], axis=0))
-            y_test.extend(y.take(folds[test_index_fold], axis=0))
+            x_test = x.take(folds[test_index_fold], axis=0)
+            y_test = y.take(folds[test_index_fold], axis=0)
 
-            yield X_train, X_test, y_train, y_test
+            yield x_train, x_test, y_train, y_test
 
 
 def main():
@@ -67,12 +65,7 @@ def main():
     y = df.quality.apply(lambda quality: 0 if quality <= 5 else 1).values
     X = MinMaxScaler().fit_transform(df.iloc[:, :-1])
 
-    # for index in [3, 5, 8]:
-    #     kfold = Kfold(index)
-    #     folds = kfold.generate_folds(y)
-    #     with open(f"../../tests/mocks/values_splitted_in_{index}_folds", "wb") as file:
-    #         pickle.dump(folds, file)
-
+    #for x_train, x_test, y_train, y_test
 
 if __name__ == "__main__":
     main()
